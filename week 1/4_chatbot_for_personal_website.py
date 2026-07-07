5ccf71cfaec07e4da0284cea41abd72d254e9e2a
unknown <me@ahmedmuneeb.com>
2026-07-07 20:04:58 +0500

    Week 1 Day 4: Add Message function

--- week 1/4_chatbot_for_personal_website.py
+++ week 1/4_chatbot_for_personal_website.py
@@ -5,22 +5,27 @@ import os
 from pypdf import PdfReader
 import csv
 import json
+import resend
 
 load_dotenv()
 
-openai = OpenAI(base_url=os.getenv("OPENAI_BASE_URL"), api_key=os.getenv("OPENAI_API_KEY"))
-model="@cf/moonshotai/kimi-k2.6"
+openai = OpenAI(
+    base_url=os.getenv("OPENAI_BASE_URL"), api_key=os.getenv("OPENAI_API_KEY")
+)
+model = "@cf/moonshotai/kimi-k2.6"
+
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 profile_pdf_reader = PdfReader("week 1/assets/profile.pdf")
 
-profile_details=""
+profile_details = ""
 for page in profile_pdf_reader.pages:
     profile_details += page.extract_text() + "\n"
-    
+
 with open("week 1/assets/details.txt", "r", encoding="utf-8") as file:
     additional_profile_details = file.read()
-    
-system_prompt=f"""You are an AI chatbot for a personal portfolio website for a person. You have to reply to the conversations/questions on his behalf. No extra huge content or descriptions, keep short and concise. Dont answer bindly. Only answer from details provided and you can say "I dont know if you dont have the context. Use a natural tone not like a bot and only answer related to that person. You are not that person you are actually an assistant of them." 
+
+system_prompt = f"""You are an AI chatbot for a personal portfolio website for a person. You have to reply to the conversations/questions on his behalf. No extra huge content or descriptions, keep short and concise. Dont answer bindly. Only answer from details provided and you can say "I dont know if you dont have the context. Use a natural tone not like a bot and only answer related to that person. You are not that person you are actually an assistant of them." 
 
 # Person Details:
 {profile_details}
@@ -38,88 +43,149 @@ tools = [
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "name": {
-                        "type": "string",
-                        "description": "Subscriber name"
-                    },
+                    "name": {"type": "string", "description": "Subscriber name"},
                     "email": {
                         "type": "string",
-                        "description": "Email address to subscribe"
-                    }
+                        "description": "Email address to subscribe",
+                    },
                 },
                 "required": ["email", "name"],
-                "additionalProperties": False
-            }
-        }
+                "additionalProperties": False,
+            },
+        },
     },
     {
         "type": "function",
         "function": {
             "name": "get_resume",
             "description": "This tool give the resume url",
-        }
-    }
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "contact_message",
+            "description": "This tool send an email to the owner. use this as a contact method.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": "Name of the person who wants to contact (visitor)",
+                    },
+                    "email": {
+                        "type": "string",
+                        "format": "email",
+                        "description": "Email of the person who wants to contact (visitor)",
+                    },
+                    "subject": {
+                        "type": "string",
+                        "description": "A short email subject generated from the message. Do not ask the user for a subject.",
+                    },
+                    "message": {
+                        "type": "string",
+                        "description": "Messgae that the visitor wants to send.",
+                    },
+                },
+                "required": ["email", "name", "subject", "message"],
+                "additionalProperties": False,
+            },
+        },
+    },
 ]
 
+
 def handle_subscribe_newsletter(name, email):
-    csv_path="week 1/assets/newsletter.csv"
+    csv_path = "week 1/assets/newsletter.csv"
     with open(csv_path, "a", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=["name", "email"])
-        
+
         if os.path.getsize(csv_path) == 0:
             writer.writeheader()
-            
-        writer.writerow({
-            "name": name,
-            "email": email
-        })
-    
-    return {
-        "success": True
-    }
-    
+
+        writer.writerow({"name": name, "email": email})
+
+    return {"success": True}
+
+
 def handle_get_resume():
-    return {
-        "success": True,
-        "url": "https://example.com/resume.pdf"
-    }
+    return {"success": True, "url": "https://example.com/resume.pdf"}
+
+
+def handle_contact_message(name, email, subject, message):
+    html = f"""
+	<h2>New Message from Chatbot</h2>
+
+	<p><strong>Name:</strong> {name}</p>
+	<p><strong>Email:</strong> {email}</p>
+
+	<p><strong>Message:</strong></p>
+	<p>{message}</p>
+	"""
+
+    resend.Emails.send(
+        {
+            "from": os.getenv("RESEND_SENDER_EMAIL"),
+            "to": os.getenv("RESEND_USER_EMAIL"),
+            "subject": subject,
+            "html": html,
+        }
+    )
+
+    return {"sucess": True}
+
 
 tool_box = {
     "subscribe_newsletter": handle_subscribe_newsletter,
     "get_resume": handle_get_resume,
+    "contact_message": handle_contact_message,
 }
 
+
 def chat(message, history):
-    messages = [{"role": "system", "content": system_prompt}] + history + [{"role": "user", "content": message}]
-    
+    messages = (
+        [{"role": "system", "content": system_prompt}]
+        + history
+        + [{"role": "user", "content": message}]
+    )
+
     res = openai.chat.completions.create(model=model, messages=messages, tools=tools)
-    
+
     while res.choices[0].finish_reason == "tool_calls":
         message = res.choices[0].message
         tool_calls = message.tool_calls
-        
-        results=[]
-        
+
+        results = []
+
         for tool_call in tool_calls:
             tool_name = tool_call.function.name
             tool_arguments = json.loads(tool_call.function.arguments)
-            
-            tool=tool_box[tool_name]
-            
-            result=""
-            
+
+            tool = tool_box[tool_name]
+
+            result = ""
+
             if tool:
                 result = tool(**tool_arguments)
             else:
-                result= "Tool Not found"
-                
-            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
-        
+                result = {"success": False, "message": "Tool Not found"}
+
+            results.append(
+                {
+                    "role": "tool",
+                    "content": json.dumps(result),
+                    "tool_call_id": tool_call.id,
+                }
+            )
+
         messages.append(message)
         messages.extend(results)
-        
-        res = openai.chat.completions.create(model=model, messages=messages, tools=tools)
-    
+
+        res = openai.chat.completions.create(
+            model=model, messages=messages, tools=tools
+        )
+
     return res.choices[0].message.content
+
 
 gr.ChatInterface(chat).launch(inbrowser=True)
